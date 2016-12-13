4f7133d9761f7e8ea1665b91ca6beb5b999bd826
rafaeldrs <rafriosan@gmail.com>
2016-12-13 11:38:08 +0100

    Initial commit

---
+++ apex-blackops/functions/write/index.js
@@ -0,0 +1,28 @@
+console.log('starting function');
+
+const AWS = require('aws-sdk'),
+    docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-central-1'});
+
+exports.handle = function (e, ctx, callback) {
+
+    var params = {
+        Item: {
+            map: e.map,
+            weapon: e.weapon,
+            points: e.points,
+            mode: e.mode,
+            date: e.date
+        },
+
+        TableName: 'blackops'
+    };
+
+    docClient.put(params, function(err, data) {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, data);
+        }
+    });
+
+}
---
+++ apex-dynamo-write/functions/read/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+console.log('starting function');
+
+const AWS = require('aws-sdk'),
+    docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-central-1'});
+
+exports.handle = function (e, ctx, callback) {
+
+    // scanningParameters no es eficiente, se deberia usar query
+    
+    let scanningParameters = {
+        TableName: 'guestbook',
+        Limit: 100
+    };
+
+    docClient.scan(scanningParameters, function(err, data) {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, data);
+        }
+    });
+    /*
+    var params = {
+        TableName: 'guestbook',
+        Key: {
+            "date": 1481557571126
+        }
+    }
+
+    docClient.get(params, function(err, data) {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, data);
+        }
+    });
+    */
+}
